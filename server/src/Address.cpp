122def75b069f1c99c20d0f2182116c22871ae83
jadenbn <jlambn@shaw.ca>
2026-08-09 14:42:54 -0700

    add address body

---
+++ server/src/Address.cpp
@@ -0,0 +1,25 @@
+#include "Address.h"
+
+Address::Address() {
+  port = 0;
+  address = 0;
+}
+
+Address::Address(unsigned char a, unsigned char b, unsigned char c,
+                 unsigned char d, unsigned short port) {
+  address = ((a << 24) | (b << 16) | (c << 8) | d);
+  this->port = port;
+}
+
+Address::Address(unsigned int address, unsigned short port) {
+  this->address = address;
+  this->port = port;
+}
+
+unsigned int Address::GetAddress() const { return address; }
+
+unsigned char Address::GetA() const { return (unsigned char)(address >> 24); }
+unsigned char Address::GetB() const { return (unsigned char)(address >> 16); }
+unsigned char Address::GetC() const { return (unsigned char)(address >> 8); }
+unsigned char Address::GetD() const { return (unsigned char)(address); }
+unsigned short Address::GetPort() const { return port; }
