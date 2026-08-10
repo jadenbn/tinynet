ba799511dbd7ab55b89846547359774131c11d4d
jadenbn <jlambn@shaw.ca>
2026-08-10 13:40:18 -0700

    reading methods

--- server/src/Packet.cpp
+++ server/src/Packet.cpp
@@ -32,3 +32,32 @@ void WriteChar(Buffer &buff, uint8_t data) {
 
   buff.index += 1;
 };
+
+uint32_t ReadInteger(Buffer &buff) {
+  assert(buff.index + 4 <= buff.size);
+
+  uint8_t b4 = buff.data[buff.index + 3];
+  uint8_t b3 = buff.data[buff.index + 2];
+  uint8_t b2 = buff.data[buff.index + 1];
+  uint8_t b1 = buff.data[buff.index + 0]; // msb
+  buff.index += 4;
+  return (static_cast<uint32_t>(b4) << 24 | static_cast<uint32_t>(b3) << 16) |
+         static_cast<uint32_t>(b2) << 8 | static_cast<uint32_t>(b1);
+}
+
+uint16_t ReadShort(Buffer &buff) {
+  assert(buff.index + 2 <= buff.size);
+
+  uint8_t b2 = buff.data[buff.index + 1];
+  uint8_t b1 = buff.data[buff.index + 0]; // msb
+  buff.index += 2;
+  return static_cast<uint32_t>(b2) << 8 | static_cast<uint32_t>(b1);
+}
+
+uint8_t ReadChar(Buffer &buff) {
+  assert(buff.index + 1 <= buff.size);
+
+  uint8_t b1 = buff.data[buff.index + 0];
+  buff.index += 1;
+  return b1;
+}
