866cf237f04742b38060d69feeda6673eae59311
jadenbn <jlambn@shaw.ca>
2026-08-10 13:52:14 -0700

    add read an dfix endianness

--- server/src/Packet.cpp
+++ server/src/Packet.cpp
@@ -36,10 +36,10 @@ void WriteChar(Buffer &buff, uint8_t data) {
 uint32_t ReadInteger(Buffer &buff) {
   assert(buff.index + 4 <= buff.size);
 
-  uint8_t b4 = buff.data[buff.index + 3];
-  uint8_t b3 = buff.data[buff.index + 2];
-  uint8_t b2 = buff.data[buff.index + 1];
-  uint8_t b1 = buff.data[buff.index + 0]; // msb
+  uint8_t b1 = buff.data[buff.index + 3];
+  uint8_t b2 = buff.data[buff.index + 2];
+  uint8_t b3 = buff.data[buff.index + 1];
+  uint8_t b4 = buff.data[buff.index + 0]; // msb
   buff.index += 4;
   return (static_cast<uint32_t>(b4) << 24 | static_cast<uint32_t>(b3) << 16) |
          static_cast<uint32_t>(b2) << 8 | static_cast<uint32_t>(b1);
@@ -48,8 +48,8 @@ uint32_t ReadInteger(Buffer &buff) {
 uint16_t ReadShort(Buffer &buff) {
   assert(buff.index + 2 <= buff.size);
 
-  uint8_t b2 = buff.data[buff.index + 1];
-  uint8_t b1 = buff.data[buff.index + 0]; // msb
+  uint8_t b1 = buff.data[buff.index + 1];
+  uint8_t b2 = buff.data[buff.index + 0]; // msb
   buff.index += 2;
   return static_cast<uint32_t>(b2) << 8 | static_cast<uint32_t>(b1);
 }
---
+++ server/tests/test_packet_helpers.cpp
@@ -0,0 +1,33 @@
+#include "../src/Packet.h"
+#include <cassert>
+#include <charconv>
+#include <cstdint>
+#include <iostream>
+
+void test_all() {
+  Buffer buff;
+  uint8_t data[128];
+  buff.size = 128;
+  buff.index = 0;
+  buff.data = data;
+
+  WriteInteger(buff, 16161616);
+  assert(buff.index == 4);
+
+  WriteShort(buff, 24213);
+  assert(buff.index == 6);
+
+  WriteChar(buff, 4);
+  assert(buff.index == 7);
+
+  buff.index = 0;
+  assert(ReadInteger(buff) == 16161616);
+  assert(ReadShort(buff) == 24213);
+  assert(ReadChar(buff) == 4);
+  assert(buff.index == 7);
+}
+
+int main() {
+  test_all();
+  std::cout << "All tests passed!" << std::endl;
+}
